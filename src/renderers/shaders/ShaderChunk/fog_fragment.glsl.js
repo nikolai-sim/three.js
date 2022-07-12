171e8735633b44337aec6bdfe6594c5e5473cf2e
nikolai-sim <89240062+nikolai-sim@users.noreply.github.com>
2022-07-13 10:56:13 +1200

    Update fog_fragment.glsl.js

--- src/renderers/shaders/ShaderChunk/fog_fragment.glsl.js
+++ src/renderers/shaders/ShaderChunk/fog_fragment.glsl.js
@@ -1,17 +1,20 @@
 export default /* glsl */`
 #ifdef USE_FOG
+      vec3 fogOrigin = cameraPosition;
+      vec3 fogDirection = normalize(vWorldPosition - fogOrigin);
+      float fogDepth = distance(vWorldPosition, fogOrigin);
 
-	#ifdef FOG_EXP2
+      // f(p) = fbm( p + fbm( p ) )
+      vec3 noiseSampleCoord = vWorldPosition * 0.00025 + vec3(
+          0.0, 0.0, fogTime * 0.025);
+      float noiseSample = FBM(noiseSampleCoord + FBM(noiseSampleCoord)) * 0.5 + 0.5;
+      fogDepth *= mix(noiseSample, 1.0, saturate((fogDepth - 5000.0) / 5000.0));
+      fogDepth *= fogDepth;
 
-		float fogFactor = 1.0 - exp( - fogDensity * fogDensity * vFogDepth * vFogDepth );
+      float heightFactor = 0.05;
+      float fogFactor = heightFactor * exp(-fogOrigin.y * fogDensity) * (
+          1.0 - exp(-fogDepth * fogDirection.y * fogDensity)) / fogDirection.y;
+      fogFactor = saturate(fogFactor);
 
-	#else
-
-		float fogFactor = smoothstep( fogNear, fogFar, vFogDepth );
-
-	#endif
-
-	gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
-
-#endif
-`;
+      gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
+    #endif`;
